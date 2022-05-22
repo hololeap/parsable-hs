6a2620de007d9a5bf0f1cbe616f3b9ec473c46bf
hololeap <hololeap@users.noreply.github.com>
2022-05-22 10:14:09 -0600

    First release
    Signed-off-by: hololeap <hololeap@users.noreply.github.com>

--- src/Data/Parsable.hs
+++ src/Data/Parsable.hs
@@ -1,8 +1,40 @@
+{-|
+Module      : Data.Parsable
+
+This module contains two simple classes, 'Parsable' and 'Printable'.
+
+There is an implicit "soft isomorphism" between 'parser' and 'toString'.
+(Successfully parsing a string and then running 'toString' on the result
+should result in the original string.)
+
+'Parsable' instances must return a 'PartialParse' wrapping the data. This can
+be done easily with the 'endPartial' function.
+
+For instance:
+
+> parser = endPartial $ count 5 anyChar
+
+=== Language extensions
+
+Because 'parser' and 'parser'' do not take any arguments, it may be necessary
+to explicitly declare the type of @t@ for these functions.
+
+It may be helpful to enable and use the @TypeApplications@ and possibly
+@ScopedTypeVariables@ extensions. This extends to helper functions that use
+'parser' and 'parser'', such as 'runParsable' and 'runParsable''.
+
+Look at the @Language Extensions@ section of the GHC documentation for
+instructions on how to use these extensions.
+-}
+
 {-# Language ConstrainedClassMethods #-}
 {-# Language DefaultSignatures #-}
 {-# Language DeriveTraversable #-}
 {-# Language FlexibleContexts #-}
+{-# Language FlexibleInstances #-}
 {-# Language LambdaCase #-}
+{-# Language ScopedTypeVariables #-}
+{-# Language TypeApplications #-}
 {-# Language TypeFamilies #-}
 
 -- Needed to remove the warning when we set the default type instance
@@ -10,55 +42,62 @@
 {-# Options_GHC -Wno-unused-type-patterns #-}
 
 module Data.Parsable
-    ( Parsable(..)
+    (
+    -- * Parsing
+      Parsable(..)
+    , runParsable
+    , runParsable'
+    -- ** Partial parses
     , PartialParse(..)
     , getPartialParse
     , isCompleteParse
+    -- ** Parsing functions
     , satisfyAny
     , someAllowed
     , wordsWithSep
+    , wordsWithSep'
     , endPartial
-    , parseCabalPortage
-    , parseCabalPortage'
-    , module Control.Monad.Except
+    -- * Printing
+    , Printable(..)
+    , toText
+    -- * Re-exports
+    , module Data.Char
+    , module Data.Functor.Identity
+    , module Data.Kind
+    , module Data.String
     , module Text.Parsec
+    , module Control.Applicative
     ) where
 
 import Control.Applicative (some)
-import Control.Monad.Except
--- import Control.Monad.Reader
+import Data.Char
 import Data.Functor.Identity
 import Data.Kind
-import Text.Parsec hiding (runParser)
-
-import Distribution.Cabal.Portage.Error
+import Data.String
+import Data.Text (Text, unpack)
+import Text.Parsec
 
 -- | If a parse succeeds for the beginning of the input, but then fails, we
---   wrap the successful part in the 'PartialParse' constructor. If the entire
+--   wrap the successful part in the v'PartialParse' constructor. If the entire
 --   parse was successful, we wrap it in 'CompleteParse'.
+--
+--   This is mostly useful for testing parsers where we need to know if the
+--   entire test string was parsed or not, but still allows for combining
+--   individual parsers into larger ones.
 data PartialParse t
-    = PartialParse String t
+    = PartialParse
+        String -- ^ The unparsed remainder. Useful for error messages
+        t
     | CompleteParse t
     deriving (Show, Eq, Ord, Functor, Foldable, Traversable)
 
--- -- | TODO: This assumes combining anything with a 'PartialParse' yields a
--- --   'PartialParse', which may not be reasonable behavior.
--- instance Semigroup t => Semigroup (PartialParse t) where
---     CompleteParse t1 <> CompleteParse t2 = CompleteParse (t1 <> t2)
---     PartialParse  t1 <> CompleteParse t2 = PartialParse (t1 <> t2)
---     CompleteParse t1 <> PartialParse  t2 = PartialParse (t1 <> t2)
---     PartialParse  t1 <> PartialParse  t2 = PartialParse (t1 <> t2)
---
--- -- | TODO: Given the 'Semigroup' instance, the only thing that makes sense
--- --   for 'mempty' is 'CompleteParse'.
--- instance Monoid t => Monoid (PartialParse t) where
---     mempty = CompleteParse mempty
-
+-- | Extract the data from a 'PartialParse'
 getPartialParse :: PartialParse t -> t
 getPartialParse = \case
     PartialParse _ t -> t
     CompleteParse t -> t
 
+-- | Returns @True@ on 'CompleteParse' and @False@ on v'PartialParse'
 isCompleteParse :: PartialParse t -> Bool
 isCompleteParse = \case
     PartialParse _ _ -> False
@@ -66,6 +105,9 @@ isCompleteParse = \case
 
 -- | Represents types that have a valid Parsec parser.
 class Parsable t where
+    -- | Represents some other input that is needed to create the parser. This
+    --   defaults to @()@ and can safely be ignored in most cases, but is
+    --   available if needed.
     type family ParsableInput t :: Type
     type instance ParsableInput t = ()
 
@@ -73,60 +115,92 @@ class Parsable t where
         => Parsec s () (PartialParse t)
     parser = parser' ()
 
+    -- | A more general case of 'parser' for when @ParsableInput t@ is /not/ equal
+    --   to @()@. This must be defined instead of 'parser' if this is the case.
     parser' :: Stream s Identity Char
-        => ParsableInput t -> Parsec s () (PartialParse t)
+        => ParsableInput t -- ^ Input needed to build the 'Parsable' parser
+        -> Parsec s () (PartialParse t)
 
     default parser' :: (Stream s Identity Char, ParsableInput t ~ ())
-        => ParsableInput t -> Parsec s () (PartialParse t)
+        => ParsableInput t -- ^ Input needed to build the 'Parsable' parser
+        -> Parsec s () (PartialParse t)
     parser' _ = parser
 
     {-# Minimal parser | parser' #-}
 
--- | Helper function to wrap any parse error in a 'CabalPortageError', then
---   lift to a 'ExceptT' monad transformer.
-parseCabalPortage'
-    :: (MonadError CabalPortageError m, Stream s Identity Char, Parsable t)
-    => ParsableInput t
-    -> (ParseError -> CabalPortageError) -- ^ Constructor taking a ParseError
-    -> SourceName                        -- ^ A name for where the input comes from
-    -> s                                 -- ^ Input to parse
-    -> m (PartialParse t)
-parseCabalPortage' t c sn = liftEither . either (Left . c) Right . parse (parser' t) sn
+-- | Convenience method to run a 'Parsable' parser.
+runParsable :: forall t s. (Parsable t, Stream s Identity Char, ParsableInput t ~ ())
+    => SourceName -- ^ Only used in error messages and may be the empty string
+    -> s
+    -> Either ParseError (PartialParse t)
+runParsable = parse (parser @t)
 
-parseCabalPortage ::
-    ( MonadError CabalPortageError m
-    , Stream s Identity Char
-    , Parsable t
-    , ParsableInput t ~ ()
-    ) => (ParseError -> CabalPortageError) -- ^ Constructor taking a ParseError
-    -> SourceName                        -- ^ A name for where the input comes from
-    -> s                                 -- ^ Input to parse
-    -> m (PartialParse t)
-parseCabalPortage = parseCabalPortage' ()
+-- | Same as 'runParsable' but takes a non-trivial 'ParsableInput' as an argument.
+runParsable' :: forall t s. (Parsable t, Stream s Identity Char)
+    => ParsableInput t -- ^ Input needed to build the 'Parsable' parser
+    -> SourceName      -- ^ Only used in error messages and may be the empty string
+    -> s
+    -> Either ParseError (PartialParse t)
+runParsable' i = parse (parser' @t i)
 
 -- | Parse a Char that satisfies any of the given predicates
 satisfyAny :: Stream s Identity Char => [Char -> Bool] -> Parsec s u Char
-satisfyAny fs = satisfy $ \c -> or $ ($c) <$> fs
+satisfyAny fs = satisfy $ \c -> or [f c | f <- fs]
 
 -- | One or more characters that satisfy any of the given predicates
 someAllowed :: Stream s Identity Char
     => [Char -> Bool]
     -> Parsec s u (PartialParse String)
-someAllowed allowed = wordsWithSep Nothing allowed allowed []
+someAllowed allowed = wordsWithSep allowed allowed []
 
 -- | Best effort parsing of "words" starting with certain allowed characters,
---   then multiple of characters allowed after the start of a "word".
+--   then multiple of characters allowed after the start of a word.
 --
---   "Words" are separated by certain allowed characters, such as @'-'@ and
---   @'_'@.
+--   Words are separated by certain allowed characters, such as @\'-\'@ and
+--   @\'_\'@.
+--
+--   Example:
+--
+--   > wordsWithSep [isAsciiUpper] [isAsciiUpper, isAsciiLower] [(== '-')]
+--
+--   This would parse a string such as @"SomeAwesome-Thing"@:
+--
+--       * The first character of each word must be an upper-case ASCII letter.
+--
+--       * The remaining characters of each word may be upper- or lower-case
+--         letters.
+--
+--       * Words are separated by @'-'@ characters.
+--
+--  If a @wordsWithSep@ parser fails, it will return v'PartialParse' wrapping
+--  the string up to and including the last successfully parsed word.
 wordsWithSep :: Stream s Identity Char
+    => [Char -> Bool]    -- ^ Characters allowed at the start of a word
+    -> [Char -> Bool]    -- ^ Characters allowed after the start of a word
+    -> [Char -> Bool]    -- ^ Characters that separate words
+    -> Parsec s u (PartialParse String)
+wordsWithSep = wordsWithSepG Nothing
+
+-- | The same as 'wordsWithSep', but this takes a parser which will be run in
+--   place of normal word detection for the first word. This allows for
+--   starting with generally unallowed characters before the first separator.
+wordsWithSep' :: Stream s Identity Char
+    => Parsec s u String -- ^ Beginning parser, skips normal beginning
+    -> [Char -> Bool]    -- ^ Characters allowed at the start of a word
+    -> [Char -> Bool]    -- ^ Characters allowed after the start of a word
+    -> [Char -> Bool]    -- ^ Characters that separate words
+    -> Parsec s u (PartialParse String)
+wordsWithSep' = wordsWithSepG . Just
+
+-- | General version of 'wordsWithSep'. Not exported.
+wordsWithSepG :: Stream s Identity Char
     -- | Optional beginning of parser, skips normal beginning
     => Maybe (Parsec s u String)
-    -> [Char -> Bool]    -- ^ Character allowed at the start of a "word"
-    -> [Char -> Bool]    -- ^ Characters allowed after the start of a "word"
-    -> [Char -> Bool]    -- ^ Characters that separate "words"
+    -> [Char -> Bool]    -- ^ Characters allowed at the start of a word
+    -> [Char -> Bool]    -- ^ Characters allowed after the start of a word
+    -> [Char -> Bool]    -- ^ Characters that separate words
     -> Parsec s u (PartialParse String)
-wordsWithSep maybeBeg wordStart wordRest wordSep = do
+wordsWithSepG maybeBeg wordStart wordRest wordSep = do
     beg <- case maybeBeg of
         Just p -> p
         Nothing -> do
@@ -136,14 +210,35 @@ wordsWithSep maybeBeg wordStart wordRest wordSep = do
     choice
         [ try $ do
             sep  <- satisfyAny wordSep
-            next <- wordsWithSep Nothing wordStart wordRest wordSep
+            next <- wordsWithSepG Nothing wordStart wordRest wordSep
             pure $ ((beg ++ [sep]) ++) <$> next
-        , ($ beg) <$> endPartial
+        , endPartial $ pure beg
         ]
 
--- | Return 'CompleteParse' if we are at 'eof', otherwise 'PartialParse'
-endPartial :: Stream s Identity Char => Parsec s u (t -> PartialParse t)
-endPartial = choice
-    [ CompleteParse <$ eof
-    , PartialParse <$> lookAhead (some anyChar)
-    ]
+-- | Run the specified parser, then return 'CompleteParse' if we are at
+--   'eof', otherwise 'PartialParse'.
+endPartial :: Stream s Identity Char
+    => Parsec s u t
+    -> Parsec s u (PartialParse t)
+endPartial p = do
+    t <- p
+    f <- choice
+            [ CompleteParse <$ eof
+            , PartialParse <$> lookAhead (some anyChar)
+            ]
+    pure $ f t
+
+-- | Types that can be converted back to a @String@.
+class Printable t where
+    toString :: t -> String
+    {-# Minimal toString #-}
+
+instance Printable String where
+    toString = id
+
+instance Printable Text where
+    toString = unpack
+
+-- | Convenience method that will turn a 'Printable' to any 'IsString'.
+toText :: (Printable t, IsString s) => t -> s
+toText = fromString . toString
